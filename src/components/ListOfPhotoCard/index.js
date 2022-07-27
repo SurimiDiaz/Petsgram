d288973640432290bf6750fdd550f50bdbf458a4
SurimiDiaz <laurasurimi@gmail.com>
2022-07-27 13:18:09 -0500

    agregado graphql

--- src/components/ListOfPhotoCard/index.js
+++ src/components/ListOfPhotoCard/index.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import { photos} from "../../../api/db.json"
-import { PhotoCard } from '../PhotoCard';
+import React from "react";
+//import { photos } from "../../../api/db.json";
+import { PhotoCard } from "../PhotoCard";
+import { useQuery, gql } from "@apollo/client";
+
+const PHOTOS_QUERY = gql`
+  {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
 export const ListOfPhotoCard = () => {
+  const { data, isLoading, error } = useQuery(PHOTOS_QUERY);
+
+  if (error) {
+    return <h2>Internal Server Error</h2>;
+  }
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <ul>
-      {
-        photos.map((photo)=>{
-          return (
-            <li key={photo.id}>
-              {
-               <PhotoCard {...photo}/>
-              }
-            </li>
-          )
-        })
-      }
-    </ul>
+    <>
+      {data && (
+        <ul>
+          {data.photos.map((photo) => {
+            return <li key={photo.id}>{<PhotoCard {...photo} />}</li>;
+          })}
+        </ul>
+      )}
+    </>
   );
 };
-
- 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {App} from "./App";
+import { App } from "./App";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-root.render(<App/>);
+const client = new ApolloClient({
+  uri: "https://petgram-server-surimi-surimidiaz.vercel.app/graphql",
+  cache: new InMemoryCache(),
+});
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
